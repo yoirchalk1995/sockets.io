ab50b551b573702738f77ce9175c39077acf6859
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-04-22 23:57:35 +0300

    completed first excercise

---
+++ intro.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { Server } = require("socket.io");
+const path = require("path");
+const http = require("http");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "intro.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected:", socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("server listening on port 3000");
+});
